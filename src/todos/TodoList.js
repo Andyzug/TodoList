f99db955a77f948447a69aa08638bb103ef956ea
Andyzug <zugravu.andrei@gmail.com>
2021-11-26 18:47:52 -0500

    first commit

---
+++ src/todos/TodoList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NewTodoForm from "./NewTodoForm";
+import TodoListItem from "./TodoListItem";
+import "./TodoList.css";
+
+const TodoList = ({ todos = [{ text: "Hello" }] }) => (
+  <div className="list-wraper">
+    <NewTodoForm />
+    {todos.map((todo) => (
+      <TodoListItem todo={todo} />
+    ))}
+  </div>
+);
+
+export default TodoList;
