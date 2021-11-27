c294f64563613d1b5ea255807813d7affe402959
Andyzug <zugravu.andrei@gmail.com>
2021-11-26 19:21:41 -0500

    second commit

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -3,7 +3,7 @@ import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
 
-const TodoList = ({ todos = [{ text: "Hello" }] }) => (
+const TodoList = ({ todos = [{ text: "Wash Your hands" }] }) => (
   <div className="list-wraper">
     <NewTodoForm />
     {todos.map((todo) => (
