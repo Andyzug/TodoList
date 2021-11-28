5b81e33c3fd8d9e51640d5c98fe4314fcc76c751
Andyzug <zugravu.andrei@gmail.com>
2021-11-27 21:36:04 -0500

    able to remove todos from server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,9 +1,9 @@
 import {
   createTodo,
+  removeTodo,
   loadTodosInProgress,
   loadTodosSuccess,
   loadTodosFailure,
-  // createTodo,
 } from "./actions";
 
 export const loadTodos = () => async (dispatch, getState) => {
@@ -31,6 +31,18 @@ export const addTodoRequest = (text) => async (dispatch) => {
     });
     const todo = await response.json();
     dispatch(createTodo(todo));
+  } catch (e) {
+    dispatch(displayAlert(e));
+  }
+};
+
+export const removeTodoRequest = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(`http://localhost:8080/todos/${id}`, {
+      method: "delete",
+    });
+    const removedTodo = await response.json();
+    dispatch(removeTodo(removedTodo));
   } catch (e) {
     dispatch(displayAlert(e));
   }
