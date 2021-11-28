a09db23eb22e4611de68bcc2845f0be99e250443
Andyzug <zugravu.andrei@gmail.com>
2021-11-27 21:48:58 -0500

    able to 'mark as completed' on server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -4,6 +4,7 @@ import {
   loadTodosInProgress,
   loadTodosSuccess,
   loadTodosFailure,
+  markTodoAsCompleted,
 } from "./actions";
 
 export const loadTodos = () => async (dispatch, getState) => {
@@ -48,6 +49,20 @@ export const removeTodoRequest = (id) => async (dispatch) => {
   }
 };
 
+export const markTodoAsCompletedRequest = (id) => async (dispatch) => {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/todos/${id}/completed`,
+      {
+        method: "post",
+      }
+    );
+    const updatedTodo = await response.json();
+    dispatch(markTodoAsCompleted(updatedTodo));
+  } catch (e) {
+    dispatch(displayAlert(e));
+  }
+};
 export const displayAlert = (text) => () => {
   alert(text);
 };
