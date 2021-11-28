c15ab4a7c1507aad6641bcaef8a27caaa89a8f18
Andyzug <zugravu.andrei@gmail.com>
2021-11-28 16:59:55 -0500

    Split Completed and Incompleted todos on page with Selectors

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,7 +2,12 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import NewTodoForm from "./NewTodoForm";
 import TodoListItem from "./TodoListItem";
-import { getTodos, getTodosLoading } from "./selectors";
+import {
+  getTodos,
+  getTodosLoading,
+  getIncompletedTodos,
+  getCompletedTodos,
+} from "./selectors";
 import {
   loadTodos,
   removeTodoRequest,
@@ -11,7 +16,8 @@ import {
 import "./TodoList.css";
 
 const TodoList = ({
-  todos = [],
+  completedTodos,
+  incompleteTodos,
   onRemovePressed,
   onCompletedPressed,
   isLoading,
@@ -25,7 +31,16 @@ const TodoList = ({
   const content = (
     <div className="list-wraper">
       <NewTodoForm />
-      {todos.map((todo) => (
+      <h3>Incomplete:</h3>
+      {incompleteTodos.map((todo) => (
+        <TodoListItem
+          todo={todo}
+          onRemovePressed={onRemovePressed}
+          onCompletedPressed={onCompletedPressed}
+        />
+      ))}
+      <h3>Completed:</h3>
+      {completedTodos.map((todo) => (
         <TodoListItem
           todo={todo}
           onRemovePressed={onRemovePressed}
@@ -39,7 +54,8 @@ const TodoList = ({
 
 const mapStateToProps = (state) => ({
   isLoading: getTodosLoading(state),
-  todos: getTodos(state),
+  completedTodos: getCompletedTodos(state),
+  incompleteTodos: getIncompletedTodos(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
